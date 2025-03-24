cf29c08a6566a1151849b2812751a3543be544e8
Lucas Grimes Ceola <133826914+LGCeola@users.noreply.github.com>
2025-03-24 16:48:10 -0300

    Update jogos.js

--- routes/jogos.js
+++ routes/jogos.js
@@ -1,1 +1,17 @@
+import express from "express";
+import { getJogos, createJogos, updateJogos, deleteJogos } from "../controllers/jogo_controller.js";
 
+const router = express.Router()
+
+router.get("/", getJogos);
+
+// Rota para criar um jogo.
+router.post("/", createJogos);
+
+// Rota para atualizar um jogo.
+router.put("/:id", updateJogos);
+
+// Rota para deletar um jogo.
+router.delete("/:id", deleteJogos);
+
+export default router
