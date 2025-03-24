47b8a320adbbf13323eb14dd05d953a64494d988
Lucas Grimes Ceola <133826914+LGCeola@users.noreply.github.com>
2025-03-24 16:49:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+import express from "express";
+import cors from "cors";
+import db from "./config/database.js";
+import jogosRota from "./routes/jogos.js";
+import http from "http";
 
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+try {
+    await db.authenticate()
+    console.log("Conectado ao MySQL!");
+} catch(e) {
+    console.log("Erro ao conectar ao MySQL.");
+}
+
+const server = http.createServer(app);
+app.use("/", jogosRota);
+server.listen(5000, ()=> console.log("Executando em http://localhost:5000"));
