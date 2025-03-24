5665341185216067b1ba9e1c18e58ba829cf60ac
Lucas Grimes Ceola <133826914+LGCeola@users.noreply.github.com>
2025-03-24 16:47:14 -0300

    Update jogo_model.js

--- models/jogo_model.js
+++ models/jogo_model.js
@@ -1,1 +1,35 @@
+import { Sequelize } from "sequelize";
+import db from "../config/database.js";
 
+const DataTypes = Sequelize
+
+const jogos = db.define('jogos', {
+    ID_jogo: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+    },
+
+    nome: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+
+    plataforma: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+
+    genero: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+
+    status: {
+        type:DataTypes.STRING(50),
+        allowNull: false
+    },
+    
+})
+
+export default jogos
