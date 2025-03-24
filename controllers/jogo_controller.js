75a4127310578172165e18135e43f06b34c0a33d
Lucas Grimes Ceola <133826914+LGCeola@users.noreply.github.com>
2025-03-24 16:46:46 -0300

    Update jogo_controller.js

--- controllers/jogo_controller.js
+++ controllers/jogo_controller.js
@@ -1,1 +1,27 @@
+import db from "../config/database.js";
 
+
+export const getJogos = async(req, res) => {
+    try {
+        const jogos = await db.query("SELECT * FROM jogos")
+        res.send(jogos)
+    } catch (e) {
+        console.log("Nenhum jogo encontrado.");
+    }
+}
+
+export const createJogos = async(req, res) => {
+
+}
+
+
+export const updateJogos = async(req, res) => {
+
+}
+
+
+export const deleteJogos = async(req, res) => {
+
+}
+
+export default db
